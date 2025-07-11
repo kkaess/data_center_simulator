5464d9a02bd37cdf0c0fcea160043868f7df332f
kkaess <kwkaess@ucdavis.edu>
2025-07-11 13:05:19 -0500

    Fixed logic error and something else

--- NodeModel.py
+++ NodeModel.py
@@ -2,7 +2,7 @@ class NodeModel:
     def __init__(self, power_model):
          self.power_model = power_model
 
-    def get_power(usage: float) -> float:
+    def get_power(self,usage: float) -> float:
         if usage < 0 or usage > 1:
             raise ValueError("In NodeModel::get_power. usage must be in [0,1]")
-        return power_model(usage)
+        return self.power_model(usage)
--- generate_data.py
+++ generate_data.py
@@ -5,12 +5,14 @@ from UsageModel import UsageModel
 from NodeModel import NodeModel
 
 if __name__=="__main__":
+    # Pull parameters
     datapoints = int(sys.argv[2])
     timestamp = int(sys.argv[3])
     conn = sqlite3.connect(sys.argv[1])
     cur = conn.cursor()
-    # This could be set up via a config file, but today I'm just hard-coding it.
 
+    # Set up cpu models
+    # This could be set up via a config file, but today I'm just hard-coding it.
     linear_model_1 = NodeModel(lambda x : 0.5 + 1.5*x)
     linear_model_2 = NodeModel(lambda x : 1.25 + 0.75*x)
     quadratic_model = NodeModel(lambda x : 0.75 + 1.25*x**2)
@@ -19,18 +21,22 @@ if __name__=="__main__":
 
     node_models = [linear_model_1,linear_model_2,quadratic_model,flat_model,free_energy_model]
 
+    # Set up usage models (i.e., cpu frequency distributions
     flat_usage_model = UsageModel(random.random) # Going to use this for all nodes
 
+    # Use the data I created, because flexibility
     nodedata = cur.execute("SELECT * FROM nodes;").fetchall()
     pdudata = cur.execute("SELECT * FROM pdus;").fetchall()
 
     npdus = len(pdudata) # Kluge, going to assume 1-indexed continuous
-
+    #
     for i in range(datapoints):
         pdu_totals = [0]*npdus
         for node in nodedata:
-            deciamps = int(round(10*node_models[node[1]-1].get_power(flat_usage_model.cast())))
-            QUERY=f"INSERT INTO cpu_measurements VALUES ({timestamp},{node[0]},{deciamps});"
+            usage=flat_usage_model.cast()
+            intusage=int(round(usage*100))
+            deciamps = int(round(10*node_models[node[1]-1].get_power(usage)))
+            QUERY=f"INSERT INTO cpu_measurements VALUES ({timestamp},{node[0]},{intusage});"
             cur.execute(QUERY)
             pdu_totals[node[3]-1]+=deciamps
         conn.commit()
