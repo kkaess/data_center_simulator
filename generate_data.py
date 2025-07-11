5f3b9ab72f1e9a76e4f8456664cdeb8282bc0809
kkaess <kwkaess@ucdavis.edu>
2025-07-11 12:47:45 -0500

    Added data generating script

---
+++ generate_data.py
@@ -0,0 +1,45 @@
+import sys
+import random
+import sqlite3
+from UsageModel import UsageModel
+from NodeModel import NodeModel
+
+if __name__=="__main__":
+    datapoints = int(sys.argv[2])
+    timestamp = int(sys.argv[3])
+    conn = sqlite3.connect(sys.argv[1])
+    cur = conn.cursor()
+    # This could be set up via a config file, but today I'm just hard-coding it.
+
+    linear_model_1 = NodeModel(lambda x : 0.5 + 1.5*x)
+    linear_model_2 = NodeModel(lambda x : 1.25 + 0.75*x)
+    quadratic_model = NodeModel(lambda x : 0.75 + 1.25*x**2)
+    flat_model = NodeModel(lambda x : 2)
+    free_energy_model = NodeModel(lambda x : -2*x)
+
+    node_models = [linear_model_1,linear_model_2,quadratic_model,flat_model,free_energy_model]
+
+    flat_usage_model = UsageModel(random.random) # Going to use this for all nodes
+
+    nodedata = cur.execute("SELECT * FROM nodes;").fetchall()
+    pdudata = cur.execute("SELECT * FROM pdus;").fetchall()
+
+    npdus = len(pdudata) # Kluge, going to assume 1-indexed continuous
+
+    for i in range(datapoints):
+        pdu_totals = [0]*npdus
+        for node in nodedata:
+            deciamps = int(round(10*node_models[node[1]-1].get_power(flat_usage_model.cast())))
+            QUERY=f"INSERT INTO cpu_measurements VALUES ({timestamp},{node[0]},{deciamps});"
+            cur.execute(QUERY)
+            pdu_totals[node[3]-1]+=deciamps
+        conn.commit()
+        QUERY=f"INSERT INTO pdu_measurements VALUES"
+        for i, pdu_total in enumerate(pdu_totals):
+            QUERY+=f" ({timestamp},{i+1},1,{pdu_total}),"
+        QUERY=QUERY[:-1]+";"
+        cur.execute(QUERY)
+        conn.commit()
+        timestamp=timestamp+300
+
+    conn.close()
